9b62df2ea74bf085ab8cf265f54f5ac822092ecc
Justin Grathwohl <justin.grathwohl@gmail.com>
2025-09-05 15:36:11 -0700

    Update Start-DeviceCleanup.ps1
    Old profile cleanup, WMI repair, and empty recycle bins

--- Task Automation/Start-DeviceCleanup.ps1
+++ Task Automation/Start-DeviceCleanup.ps1
@@ -10,17 +10,18 @@ You can add additional folders to clean out in the $cleanupFolders array, simply
 All CleanMgr options have been added and enabled that currently exist as of Windows 11 24H2. Comment out the ones you don't want to cleanup. The commented out ones are for legacy systems or are valid only when certain system options are enabled.
 
 .TODO
-- Cleanup old user profiles properly
+- Get actual last used date of old profiles for cleanup
 
 .NOTES
 Author: Justin Grathwohl
-Date: 05/08/2025
-Version: 1.1
+Date: 09/05/2025
+Version: 2.0
 
 #>
 #Requires -Version 5.1
 $VerbosePreference = "Continue"
 $ProgressPreference = "SilentlyContinue"
+$ErrorActionPreference = "SilentlyContinue"
 
 #Logging directories
 $dirPath = "C:\ScriptLogging\Start-DeviceCleanup"
@@ -39,33 +40,33 @@ If (!($logBackupPathCheck)) {
 }
 
 #Date formatting for logging outputs and queries
-$logRotateDate = (Get-Date).AddDays(-7)
-$backupRotateDate = (Get-Date).AddDays(-30)
+$logBackupRotateDate = (Get-Date).AddDays(-90)
+$scriptLogRotateDate = (Get-Date).AddDays(-7)
+$logRotateDate = (Get-Date).AddDays(-90)
 $logDate = Get-Date -Format ddMMyyyy
 
 Start-Transcript -Path $dirPath\Start-DeviceCleanup-$logDate.txt
 Write-Output "Script starting at $(Get-Date)"
-Write-Output "Cleaning up old log files..."
-Get-ChildItem -Path $dirPath -Recurse -Filter *.txt | Where-Object {$_.LastWriteTime -le $backupRotateDate} | Remove-Item -Force
+Write-Output "Cleaning up old script log files..."
+Get-ChildItem -Path $dirPath -Recurse -Filter *.txt | Where-Object {$_.LastWriteTime -le $scriptLogRotateDate} | Remove-Item -Force
 Write-Output "Cleaning up old log backups..."
-Get-ChildItem -Path $logBackupPath -Recurse -Filter *.zip | Where-Object {$_.LastWriteTime -le $logRotateDate} | Remove-Item -Force
+Get-ChildItem -Path $logBackupPath -Recurse -Filter *.zip | Where-Object {$_.LastWriteTime -le $logBackupRotateDate} | Remove-Item -Force
+Write-Output "Clearing the recycle bins for all users on all drives..."
+Get-Volume | Where-Object {$_.DriveType -eq "Fixed" -and $null -ne $_.DriveLetter } | ForEach-Object { Clear-RecycleBin -DriveLetter $_.DriveLetter -Force }
+Clear-RecycleBin -Force
 Write-Output "Setting variables for CleanMgr, DISM, and SFC, and checking folders to cleanup..."
 
-<# # Create old profile cleanup flow
-$profileRegPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList\"
-$ignoredSIDs = @(
-    "S-1-5-18",`
-    "S-1-5-19",`
-    "S-1-5-20",`
-    "S-1-5-82"
-)
-
+# Create old profile cleanup flow
+# $profileRegPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList\"
+Write-Output "Cleaning up old user profiles"
 $getUnloadedProfiles = Get-CimInstance -class Win32_UserProfile | Where-Object {(!$_.Special) -and (!$_.Loaded)} | Select-Object LocalPath,SID,LastUseTime
-$getProfilePathReg = Get-ChildItem -Path $profileRegPath
-ForEach ($userProfile in $getProfilePathReg) {
-    Remove-CimInstance
-    Remove-Item
-} #>
+$thresholdDate = (Get-Date).AddMonths(-6)
+$profilesToDelete = $getUnloadedProfiles | Where-Object { $_.LastUseTime -lt $thresholdDate -or $null -eq $_.LastUseTime }
+ForEach ($userProfile in $profilesToDelete) {
+    $profilePath = $userProfile.LocalPath
+    Remove-CimInstance -ClassName Win32_UserProfile -Filter "SID='$($profile.SID)'"
+    Get-Item -Path $profilePath | Remove-Item -Force
+}
 
 # Create array for the folders to cleanup
 $cleanupFolders = @(
@@ -79,7 +80,7 @@ $cleanupFolders = @(
     "C:\Temp",`
     "C:\Windows\System32\LogFiles",`
     "C:\inetpub\logs",`
-    "C:\Windows\System32\winevt\Logs"
+    "C:\Windows\System32\winevt\Logs",`
 )
 
 # Create array for Cleanmgr registry keys
@@ -131,26 +132,34 @@ $testCleanupFoldersTable = @{
     "winSysLogs" = "C:\Windows\System32\LogFiles"
     "iisLogs" = "C:\inetpub\logs"
     "winEventLogs" = "C:\Windows\System32\winevt\Logs"
+    "sshLogs" = "C:\ProgramData\ssh\logs"
 }
 $testCleanupFolders += New-Object psobject -Property $testCleanupFoldersTable
 
 $winSysLogs = Test-Path $testCleanupFolders.winSysLogs
 $iisLogs = Test-Path $testCleanupFolders.iisLogs
 $winEventLogs = Test-Path $testCleanupFolders.winEventLogs
+$pimLogs = Test-Path $testCleanupFolders.pimLogs
+$sshLogs = Test-Path $testCleanupFolders.sshLogs
 
 If($iisLogs) {
-	Get-ChildItem "$($testCleanupFolders.iisLogs)\*.log" -Recurse -ErrorAction SilentlyContinue | Where-Object {$_.LastWriteTime -ge $rotateDate} | Compress-Archive -DestinationPath $rotatePath\cleanupFolders-IIS-$logDate.zip -Force -ErrorAction SilentlyContinue
+	Get-ChildItem "$($testCleanupFolders.iisLogs)\*.log" -Recurse | Where-Object {$_.LastWriteTime -ge $logRotateDate} | Compress-Archive -DestinationPath $logBackupPath\cleanupFolders-IIS-$logDate.zip -Force
 	Write-Output "IIS logs compressed and backed up"
 }
 
 If($winSysLogs) {
-	Get-ChildItem "$($testCleanupFolders.winSysLogs)\*.log" -Recurse -ErrorAction SilentlyContinue | Where-Object {$_.LastWriteTime -ge $rotateDate} | Compress-Archive -DestinationPath $rotatePath\cleanupFolders-winSysLogs-$logDate.zip -Force -ErrorAction SilentlyContinue
+	Get-ChildItem "$($testCleanupFolders.winSysLogs)\*.log" -Recurse | Where-Object {$_.LastWriteTime -ge $logRotateDate} | Compress-Archive -DestinationPath $logBackupPath\cleanupFolders-winSysLogs-$logDate.zip -Force
 	Write-Output "Windows system logs compressed and backed up"
 }
 
 If($winEventLogs) {
-	Get-ChildItem "$($testCleanupFolders.winEventLogs)\*.evtx" -Recurse -ErrorAction SilentlyContinue | Where-Object {$_.LastWriteTime -ge $rotateDate} | Compress-Archive -DestinationPath $rotatePath\cleanupFolders-winEventLogs-$logDate.zip -Force -ErrorAction SilentlyContinue
+	Get-ChildItem "$($testCleanupFolders.winEventLogs)\*.evtx" -Recurse | Where-Object {$_.LastWriteTime -ge $logRotateDate} | Compress-Archive -DestinationPath $logBackupPath\cleanupFolders-winEventLogs-$logDate.zip -Force
 	Write-Output "Windows Event logs compressed and backed up"
+}
+
+If ($sshLogs) {
+	Get-ChildItem "$testCleanupFolders.sshLogs\*.log" -Recurse | Where-Object {$_.LastWriteTime -ge $logRotateDate} | Compress-Archive -DestinationPath $logBackupPath\cleanupFolders-pimLogs-$logDate.zip -Force
+	Write-Output "SSH logs compressed and backed up"
 }
 
 # WBEM Repository Cleanup to help with slow logon times and other WMI-related issues
@@ -162,29 +171,46 @@ $checkWbemRepo = Invoke-Command -ScriptBlock { winmgmt /verifyrepository }
 Switch -Wildcard ($wbemRepoSizeMB) {
     {$wbemRepoSizeMB -ge 200.00 -and $checkWbemRepo -like "*is consistent"} {
         Stop-Service Winmgmt -Force
-        Get-ChildItem $wbemRepo -Recurse -ErrorAction SilentlyContinue | Compress-Archive -DestinationPath $rotatePath\cleanupFolders-wbemRepo-$logDate.zip -Force -ErrorAction SilentlyContinue
+        Get-ChildItem $wbemRepo -Recurse | Compress-Archive -DestinationPath $logBackupPath\cleanupFolders-wbemRepo-$logDate.zip -Force
         Invoke-Command -ScriptBlock {
             winmgmt /resetrepository
             winmgmt /verifyrepository
         }
+        Start-Service winmgmt
         Write-Output "WBEM Repository is consistent but is larger than allowed size and has been reset."
     }
     {$wbemRepoSizeMB -le 199.99 -and $checkWbemRepo -like "*is consistent"} { Write-Output "WBEM Respository is consistent and below allowed size." }
     {$wbemRepoSizeMB -ge 200.00 -and $checkWbemRepo -notlike "*is consistent"} {
         Stop-Service Winmgmt -Force
-        Get-ChildItem $wbemRepo -Recurse -ErrorAction SilentlyContinue | Compress-Archive -DestinationPath $rotatePath\cleanupFolders-wbemRepo-$logDate.zip -Force -ErrorAction SilentlyContinue
+        Get-ChildItem $wbemRepo -Recurse | Compress-Archive -DestinationPath $logBackupPath\cleanupFolders-wbemRepo-$logDate.zip -Force
         Invoke-Command -ScriptBlock {
             winmgmt /resetrepository
             winmgmt /verifyrepository
         }
+        Start-Service winmgmt
         Write-Output "WBEM Repository has been reset due to inconsistencies and it is larger than allowed size."
     }
     {$wbemRepoSizeMB -le 199.99 -and $checkWbemRepo -notlike "*is consistent"} {
-        Stop-Service Winmgmt -Force
-        Get-ChildItem $wbemRepo -Recurse -ErrorAction SilentlyContinue | Compress-Archive -DestinationPath $rotatePath\cleanupFolders-wbemRepo-$logDate.zip -Force -ErrorAction SilentlyContinue
+        Get-ChildItem $wbemRepo -Recurse | Compress-Archive -DestinationPath $logBackupPath\cleanupFolders-wbemRepo-$logDate.zip -Force
         Invoke-Command -ScriptBlock {
+            Stop-Service Winmgmt -Force
+            Push-Location "C:\Windows\system32\wbem"
+            $wbem = Get-ChildItem "C:\Windows\system32\wbem" -Filter *.dll
+            $wbem | ForEach-Object { RegSvr32 -s $_.FullName }
+            # Kill this random window that pops up
+            tskill wbemtest /a 2>NUL
+            scrcons.exe /RegServer
+            unsecapp.exe /RegServer
+            wbemtest.exe /RegServer
+            tskill wbemtest /a 2>NUL
+            tskill wbemtest /a 2>NUL
             winmgmt /salvagerepository
             winmgmt /verifyrepository
+            wmiadap.exe /RegServer
+            wmiapsrv.exe /RegServer
+            wmiprvse.exe /RegServer
+            Start-Service winmgmt
+            Pop-Location
         }
         Write-Output "WBEM Repository has been reset due to inconsistencies but it was not larger than allowed size."
     }
@@ -192,7 +218,8 @@ Switch -Wildcard ($wbemRepoSizeMB) {
 
 # Start folder cleanup
 ForEach ($cleanupFolder in $cleanupFolders) {
-    Get-ChildItem "$cleanupFolder\*" -Recurse -ErrorAction SilentlyContinue | Remove-Item -Recurse -Force -ErrorAction SilentlyContinue
+    Get-ChildItem "$cleanupFolder\*" -Recurse | Remove-Item -Recurse -Force
+    Get-ChildItem "C:\Windows\System32\winevt\Logs\" -Recurse -Filter Archive*.evtx | Remove-Item -Recurse -Force
 }
 Write-Output "All cleanup folders have been cleared."
 
@@ -200,10 +227,10 @@ Write-Output "Using Windows Disk Cleanup to deep clean system files, Windows Upd
 Write-Output "Setting up Windows Disk Cleanup automation settings"
 
 ForEach ($regKey in $cleanMgrRegKeys) {
-    $getRegKeyFlags = Get-ItemProperty -Path $regKey -Name StateFlags0001 -ErrorAction SilentlyContinue
+    $getRegKeyFlags = Get-ItemProperty -Path $regKey -Name StateFlags0001
     If(!$getRegKeyFlags) {
         Write-Output "Setting CleanMgr key`: $($regKey)"
-        New-ItemProperty -Path $regKey -Name StateFlags0001 -Value 2 -PropertyType DWord
+        New-ItemProperty -Path $regKey -Name StateFlags0001 -Value 2 -PropertyType DWord | Out-Null
     }
 }
 
@@ -221,7 +248,7 @@ Write-Output "SFC scan complete. Starting DISM image health restore..."
 Start-Process -FilePath Dism.exe -ArgumentList "/Online /Cleanup-Image /RestoreHealth" -WindowStyle Hidden -Wait
 
 Write-Output "Waiting for Windows Disk Cleanup, SFC, and DISM processes. Second wait neccesary as Windows Disk Cleanup, SFC, and DISM spin off separate processes."
-Get-Process -Name cleanmgr,dismhost,sfc -ErrorAction SilentlyContinue | Wait-Process
+Get-Process -Name cleanmgr,dismhost,sfc | Wait-Process
 
 Write-Output "Windows Disk Cleanup and image repair complete, reboot at the next earliest convenience."
 
